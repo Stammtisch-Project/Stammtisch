a8394b46d4e9a8ccda73bfde8e465b2aecfe4eda
Georg Windhaber <georg.windhaber@gmail.com>
2020-06-29 21:08:57 +0200

    Added input which console logs it on submit 😎

--- Stammtisch/App.js
+++ Stammtisch/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [helloWorldMessage, setHelloWorldMessage] = useState("");
+
+	// TODO: query and set the helloWorldMessage from the server
+
+	function sendMessage() {
+		// TODO: don't log it, but write it to the server
+		console.log(helloWorldMessage);
+		setHelloWorldMessage("");
+	}
+
+	return (
+		<View style={styles.container}>
+			<TextInput
+				placeholder="Enter hello world message"
+				onChangeText={(text) => setHelloWorldMessage(text)}
+				value={helloWorldMessage}
+			></TextInput>
+			<Button onPress={sendMessage} title="Send message (to console)"></Button>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#ffffff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
