db0811b74fc301ee3b2a2da148cf1ac8e41f59f9
Georg Windhaber <georg.windhaber@gmail.com>
2020-07-02 20:20:44 +0200

    Request server message initially and re-request it after send

--- Stammtisch/App.js
+++ Stammtisch/App.js
@@ -1,30 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
 	const [helloWorldMessage, setHelloWorldMessage] = useState("");
+	const [serverMessage, setServerMessage] = useState("");
 
 	// fetch the helloWorldMessage from the server and set it
-	fetch("http://localhost:8090/hello-stammtisch")
-		.then((response) => response.text())
-		.then((text) => {
-			setHelloWorldMessage(text);
-		});
+	useEffect(() => {
+		fetchMessage();
+	}, []);
 
 	function sendMessage() {
 		// TODO: don't log it, but write it to the server
-		console.log(helloWorldMessage);
+		fetch("http://localhost:8090/hello-stammtisch", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ message: helloWorldMessage }),
+		}).then(() => {
+			fetchMessage();
+		});
 		setHelloWorldMessage("");
+	}
+
+	function fetchMessage() {
+		fetch("http://localhost:8090/hello-stammtisch")
+			.then((response) => response.text())
+			.then((text) => {
+				setServerMessage(text);
+			});
 	}
 
 	return (
 		<View style={styles.container}>
+			<h3>Server says: {serverMessage}</h3>
 			<TextInput
 				placeholder="Enter hello world message"
 				onChangeText={(text) => setHelloWorldMessage(text)}
 				value={helloWorldMessage}
 			></TextInput>
-			<Button onPress={sendMessage} title="Send message (to console)"></Button>
+			<Button onPress={sendMessage} title="Send message to server"></Button>
 		</View>
 	);
 }
