21c5e8d031748fdb55fa8e68dfa6479c284add15
Georg Windhaber <georg.windhaber@gmail.com>
2020-07-16 20:02:56 +0200

    Removed unused parameter

--- Stammtisch/utils/waiter.js
+++ Stammtisch/utils/waiter.js
@@ -3,14 +3,14 @@ const config = require("./config");
 // The waiter brings the good stuff to the customers
 // aka it's the server connection
 module.exports = {
-	get: async function (route, parameters) {
+	get: async function (route) {
 		const response = await fetch(
 			`${config.hostname}:${config.port}${route}`
 		);
 		const data = await response.text();
 		return data;
 	},
-	post: async function (route, message, parameters) {
+	post: async function (route, message) {
 		const response = await fetch(
 			`${config.hostname}:${config.port}${route}`,
 			{
